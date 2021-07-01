de175ed65faa0fc8ccf34d1450567ced1ad2c016
Infinite <infinite.hacker@protonmail.com>
2021-07-02 04:04:28 +0600

    Add splash screen

---
+++ app/src/main/java/club/thepenguins/android/activities/SplashActivity.java
@@ -0,0 +1,26 @@
+package club.thepenguins.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import club.thepenguins.android.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}

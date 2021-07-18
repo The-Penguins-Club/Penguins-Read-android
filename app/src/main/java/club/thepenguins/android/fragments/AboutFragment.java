4cc3ce282f0a925229841b3d2613dcb5cca79d70
Infinite <infinite.hacker@protonmail.com>
2021-07-19 03:08:53 +0600

    Add about page layout

--- app/src/main/java/club/thepenguins/android/fragments/AboutFragment.java
+++ app/src/main/java/club/thepenguins/android/fragments/AboutFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import club.thepenguins.android.R;
@@ -48,9 +50,15 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        ListView listView = rootView.findViewById(R.id.aboutLV);
+        String[] listItem = getResources().getStringArray(R.array.thanksTo);
+
+
+        listView.setAdapter(new ArrayAdapter<>(rootView.getContext(), R.layout.thanks, R.id.people, listItem));
+        listView.setDivider(null);
+
 
         Toasty.info(rootView.getContext(), noImplemented, Toast.LENGTH_LONG).show();
         return rootView;

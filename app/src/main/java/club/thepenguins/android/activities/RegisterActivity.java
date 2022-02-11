97b7f4e806bf987eb76fb30fa8f2b2fcafc407a3
Infinite <infinite.hacker@protonmail.com>
2022-02-11 23:46:31 +0600

    Update dependencies

---
+++ app/src/main/java/club/thepenguins/android/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package club.thepenguins.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import club.thepenguins.android.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
